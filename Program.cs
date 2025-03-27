671bcac2367fd5f98ab5000221f35f5b42ec30a6
BarossMazsi <szilagyi.istvan.marcell.1204@dszcbaross.edu.hu>
2025-03-27 10:55:38 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Eutazas
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] f = File.ReadAllLines("utasadat.txt");
+            List<utasadat> utasadatok = new List<utasadat>();
+
+            for (int i = 0; i < f.Length; i++)
+            {
+                utasadatok.Add(new utasadat(f[i]));
+            }
+
+
+            Console.WriteLine("2.Feladat.");
+            Console.WriteLine($"A buszra {utasadatok.Count} ember szeretett volna felszállni.");
+
+
+            Console.WriteLine("3.Feladat.");
+            
+           
+            Console.WriteLine("4.Feladat.");
+            var felszallas = utasadatok.GroupBy(x => x.megallo).OrderByDescending(x => x.Count()).ToList();
+            Console.WriteLine($"A legtőbb utas a {felszallas[0].Count()} megállóban próbált meg felszállni.");
+
+
+            Console.WriteLine("5.Feladat.");
+            var ingyen = utasadatok.Where(x=>x.tipus =="RVS" || x.tipus == "GYK" || x.tipus == "NYP").Count();
+            var kedvezmenyes = utasadatok.Where(x => x.tipus == "TAB" || x.tipus == "NYB").Count();
+            Console.WriteLine($"Ingyenesen utaztók száma :{ingyen}");
+            Console.WriteLine($"Kedvezményesen utazók száma : {kedvezmenyes}");
+
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ utasadat.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eutazas
+{
+    public class utasadat
+    {
+
+        public int megallo { get; set; }
+        public string datum { get; set; }
+        public int azon { get; set; }
+        public string tipus { get; set; }
+        public int ervenyesseg { get; set; }
+
+        public utasadat(string sor)
+        {
+            string[] sz = sor.Split(' ');
+            megallo= Convert.ToInt32(sz[0]);
+            datum= sz[1];
+            azon= Convert.ToInt32(sz[2]);
+            tipus = sz[3];
+            ervenyesseg= Convert.ToInt32(sz[4]);
+
+
+        }
+}
+}
